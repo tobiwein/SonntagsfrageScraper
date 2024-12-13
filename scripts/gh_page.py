974f3f73643c6a10ec7725adba21b8b29728e72e
Tobias <51930902+tobiwein@users.noreply.github.com>
2024-12-13 18:20:34 +0000

    create html from data

--- scripts/gh_page.py
+++ scripts/gh_page.py
@@ -2,12 +2,31 @@
 
 """
 
+import pandas as pd
 import plotly.express as px
-import database as db
+from scripts import database as db
+from dotenv import load_dotenv
+import os
 
-df = db.load_party_data('./data/date_data.txt')
+load_dotenv("var.env")
+DATA_POINTS = int(os.getenv("DATA_POINTS", 10))
 
-fig = px.line(df, x='x', y='y', title='Sonntagsfrage Trends')
-fig.write_html('index.html')
+def create_html_from_data(file):
+    party_data = db.load_party_data(file)
+    
+    # Transform the party_data to a format suitable for plotting
+    transformed_data = []
+    for party, date_data in party_data.items():
+        for date, values in date_data.items():
+            transformed_data.append({"date": date, "party": party, "min": values[0], "max": values[1]})
 
-print(fig.to_html())
+    # Convert the transformed data to a DataFrame
+    df = pd.DataFrame(transformed_data)
+
+    # Sort the DataFrame by date and get the last X data points
+    df['date'] = pd.to_datetime(df['date'], format='%d.%m.%Y')
+    df = df.sort_values(by='date').tail(DATA_POINTS)
+
+    # Create the plot
+    fig = px.line(df, x='date', y=['min', 'max'], color='party', title='Sonntagsfrage Trends')
+    fig.write_html('public/index.html')
