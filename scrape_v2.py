32b2d0f396b57333c69b85259655f46912cd8346
Tobias <51930902+tobiwein@users.noreply.github.com>
2024-12-15 13:50:13 +0000

    add file names

--- scrape_v2.py
+++ scrape_v2.py
@@ -14,25 +14,25 @@ logging.basicConfig(level=logging.INFO)
 url = os.getenv("SURVEY_URL")
 file = os.getenv("DATABASE_FILE")
 
-surveyer_urls = [
-    {"name": "Allensbach", "url": "https://www.wahlrecht.de/umfragen/allensbach.htm"},
-    {"name": "Verian (Emnid)", "url": "https://www.wahlrecht.de/umfragen/emnid.htm"},
-    {"name": "Forsa", "url": "https://www.wahlrecht.de/umfragen/forsa.htm"},
-    {"name": "Forschungsgruppe Wahlen", "url": "https://www.wahlrecht.de/umfragen/politbarometer.htm"},
-    {"name": "GMS", "url": "https://www.wahlrecht.de/umfragen/gms.htm"},
-    {"name": "Infratest dimap", "url": "https://www.wahlrecht.de/umfragen/dimap.htm"},
-    {"name": "INSA", "url": "https://www.wahlrecht.de/umfragen/insa.htm"},    
-    {"name": "YouGov", "url": "https://www.wahlrecht.de/umfragen/yougov.htm"},
+surveyers = [
+    {"name": "Allensbach", "url": "https://www.wahlrecht.de/umfragen/allensbach.htm", "file": "./data/surveyer/allensbach.txt"},
+    {"name": "Verian (Emnid)", "url": "https://www.wahlrecht.de/umfragen/emnid.htm", "file": "./data/surveyer/emnid.txt"},
+    {"name": "Forsa", "url": "https://www.wahlrecht.de/umfragen/forsa.htm", "file": "./data/surveyer/forsa.txt"},
+    {"name": "Forschungsgruppe Wahlen", "url": "https://www.wahlrecht.de/umfragen/politbarometer.htm", "file": "./data/surveyer/politbarometer.txt"},
+    {"name": "GMS", "url": "https://www.wahlrecht.de/umfragen/gms.htm", "file": "./data/surveyer/gms.txt"},
+    {"name": "Infratest dimap", "url": "https://www.wahlrecht.de/umfragen/dimap.htm", "file": "./data/surveyer/dimap.txt"},
+    {"name": "INSA", "url": "https://www.wahlrecht.de/umfragen/insa.htm", "file": "./data/surveyer/insa.txt"},    
+    {"name": "YouGov", "url": "https://www.wahlrecht.de/umfragen/yougov.htm", "file": "./data/surveyer/yougov.txt"},
 ]
 
 def main():
     try:
-        for surveyer in surveyer_urls:
+        for surveyer in surveyers:
             surveyer_name = surveyer["name"]
             logging.info(f"Extracting data from {surveyer_name}...")
             surveyer_data = survey_scrape.extract_surveyer_data(surveyer["url"])
             print(surveyer_data)
-            #db.store_surveyer_data(surveyer_data, f"./data/v2/{surveyer_name}.txt")
+            #db.store_surveyer_data(surveyer_data, surveyer["file"])
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
